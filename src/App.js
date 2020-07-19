abc270a98ecce9983f700938a6e4308d9d459f00
Anderson Alexander Contreras <48110006+alexandearko@users.noreply.github.com>
2020-07-19 13:41:42 -0400

    probando un back local

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-import Navigation from './components/Navigation'
+import { todos } from "./todos.json";
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <img src={logo} className="App-logo" alt="logo" />
-    </div>
-  );
+import Navigation from "./components/Navigation";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      todos,
+    };
+  }
+  render() {
+    const todos = this.state.todos.map((todo, i) => {
+      return (
+        <div className="col-md-4">
+          <div className="card mt-4">
+            <div className="card-header">
+              <h3>{todo.title}</h3>
+              <span className="bagde badge-pill badge-danger ml-2">
+                {todo.priority}
+              </span>
+            </div>
+            <div className="card-body">
+              <p>{todo.description}</p>
+              <p>{todo.responsable}</p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+    return (
+      <div className="App">
+        <Navigation />
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <div className="container">
+          <div className="row mt4">{todos}</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Navigation extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark bg-dark">
-        <a href="" className="text-white">Tasks</a>
+        <a href="" className="text-white">
+          Tasks
+        </a>
+        <span className="badge badge-pill badge-light ml-2">4</span>
       </nav>
-    )
+    );
   }
 }
 
